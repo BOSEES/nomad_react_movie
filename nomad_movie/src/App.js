8ea44b33b97f4bf89f75e1206c5d1406f228d1d9
김영일 <rladuddlf3@naver.com>
2020-09-06 20:47:02 +0900

    nomad_react_lifeCycle

--- nomad_movie/src/App.js
+++ nomad_movie/src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import PropTypes from "prop-types";
+
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
+
   state = {
     count : 0
   }
   
   add = () => {
-    console.log("add");
+    this.setState(current => ({ count : current.count + 1 }));
   }
   minus = () => {
-    console.log("minus")
+    this.setState(current => ({ count : current.count - 1 }));
   }
-
+  componentDidMount() {
+    console.log("rendered");
+  }
+  componentDidUpdate() {
+    console.log("i just update");
+  }
   render() {
+    console.log("i'm rendering");
     return(
       <div>
         <h1>The Number is : {this.state.count} </h1>
