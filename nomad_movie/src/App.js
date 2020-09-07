48779f672782ab51a8613afe6317a75cfefb166e
김영일 <rladuddlf3@naver.com>
2020-09-07 23:01:40 +0900

    nomad_react_movie_rendering

--- nomad_movie/src/App.js
+++ nomad_movie/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Axios from "axios";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
@@ -7,15 +8,25 @@ class App extends React.Component {
     movies : []
   };
   getMovies = async () => {
-    const movies = await Axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {data : { data : { movies }}} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState( { movies , isLoading : false } );
   }
 
   componentDidMount(){
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "let's begin"}</div>
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? "Loading..." : movies.map(movie => (
+    <Movie
+        key={movie.id}
+        id={movie.id}
+        year={movie.year} 
+        title={movie.title} 
+        summary={movie.summary} 
+        poster={movie.medium_cover_image}>
+      </Movie>
+    ))}</div>
   }
 }
 
