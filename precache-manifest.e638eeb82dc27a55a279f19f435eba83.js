f5c3dc210dfa1c918e19f29f424302540638eebb
김영일 <rladuddlf3@naver.com>
2020-09-27 19:16:11 +0900

    Updates

---
+++ precache-manifest.e638eeb82dc27a55a279f19f435eba83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a7163201cdd66e60c7ae0738dd0930",
+    "url": "/nomad_react_movie/index.html"
+  },
+  {
+    "revision": "7e3a0a1c357361b896db",
+    "url": "/nomad_react_movie/static/css/main.1a6dc005.chunk.css"
+  },
+  {
+    "revision": "448a7308f0a0bd5092b8",
+    "url": "/nomad_react_movie/static/js/2.0754341e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomad_react_movie/static/js/2.0754341e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e3a0a1c357361b896db",
+    "url": "/nomad_react_movie/static/js/main.b81f5db1.chunk.js"
+  },
+  {
+    "revision": "75e2d9b883adf2bb2bf1",
+    "url": "/nomad_react_movie/static/js/runtime-main.bcd64ab7.js"
+  }
+]);
